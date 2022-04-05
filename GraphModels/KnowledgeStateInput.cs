d2052f8de52c246b6417fd004b6806e8194c2da4
Andrew Edmonds <sales@docandys.com>
2022-04-05 15:01:34 +0100

    Added conversion from KSInput to KS

--- GraphModels/KnowledgeStateInput.cs
+++ GraphModels/KnowledgeStateInput.cs
@@ -12,5 +12,19 @@ namespace ThinkBase.Client.GraphModels
         public bool transient { get; set; } = false;
         public List<StringListGraphAttributeInputPair> data { get; set; } = new List<StringListGraphAttributeInputPair>();
 
+        public KnowledgeState Convert()
+        {
+            var ks = new KnowledgeState { created = DateTime.UtcNow, subjectId = subjectId, knowledgeGraphName = knowledgeGraphName, data = new Dictionary<string, List<GraphAttribute>>() };
+            foreach(var pair in data)
+            {
+                ks.data.Add(pair.name, new List<GraphAttribute>());
+                foreach(var a in pair.value)
+                {
+                    ks.data[pair.name].Add(new GraphAttribute { value = a.value, confidence = a.confidence ?? 0.0, existence = a.existence, lineage = a.lineage, name = a.name, type = a.type });
+                }
+            }
+            return ks;
+        }
+
     }
 }
